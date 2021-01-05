49f02c02ef6fb63328e5ca35a499375da1164bc1
komukomu900 <z9l2c26@gmail.com>
2021-01-05 13:29:43 +0900

    fixes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,6 @@
 class UsersController < ApplicationController
   before_action :check_current_user, only: [:edit, :update, :destroy]
   before_action :set_user, only: [:edit, :update, :destroy, :show, :favorite, :post_index]
-
   def new
     @user = User.new
   end
